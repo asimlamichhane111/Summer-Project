c7e83da426b2b6bfe387bea79187f2eebfeb053e
asimlamichhane111 <asimlamichhane000@gmail.com>
2025-03-11 15:05:45 +0545

    Delete src directory

--- src/data/product.jsx
+++
@@ -1,6 +0,0 @@
-const products=[
-    {id:1,name:"Johnnie Walker Black",price:5000,category:'rum',image:'/images/johnnie.jpg'},
-    {id:2,name:"Khukuri Rum",price:1800,category:'rum',image:'/images/khukuri.jpg'},
-    {id:3,name:"Smironff Vodka",price:3000,category:'rum',image:'/images/smirnoff.jpg'}
-];
-export default products;
